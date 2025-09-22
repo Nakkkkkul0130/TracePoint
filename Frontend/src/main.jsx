f84fddaefcbba9bce62998c07bf96e101c71b10d
nakul <nakulbhar7308@gmail.com>
2025-09-22 16:58:37 +0530

    speed insight changes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
