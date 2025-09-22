be3093173f7e4df7a5378407e0751ef2909405e9
nakul <nakulbhar7308@gmail.com>
2025-09-22 17:05:17 +0530

    speed insight

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <App />
       <Analytics />
-      <SpeedInsights />
+      <SpeedInsights url="https://trace-point.vercel.app/" framework="react" />
     </BrowserRouter>
   </React.StrictMode>
 );
